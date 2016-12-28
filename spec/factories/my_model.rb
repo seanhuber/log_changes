aed4892864a4f55ac30bdddb13c9592eef794ade
Sean Huber <seanhuber@seanhuber.com>
2016-12-28 14:40:06 -0600

    configuring Factory Girl

---
+++ spec/factories/my_model.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :my_model do
+    string_attr 'hello worldzzzzz!'
+  end
+end
--- spec/log_changes_spec.rb
+++ spec/log_changes_spec.rb
@@ -1,6 +1,12 @@
 module LogChanges
   describe Base do
+    before :each do
+      FileUtils.rm_rf(Rails.root.join('log', 'record_changes')) if File.directory? Rails.root.join('log', 'record_changes')
+    end
+
     it 'should assert the truth' do
+      FactoryGirl.create :my_model
+      puts "\n\n#{MyModel.count}\n\n"
       expect(true).to be true
     end
   end
